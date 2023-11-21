2bb0e1bd46fc49b6d6a89605b2feec61d7c138b9
siwon <ysw421@naver.com>
2023-11-21 18:22:21 +0900

    added Electron, but i can't use tts in electron :<

---
+++ electron.tsx
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('http://localhost:3000/mode');
+  // win.loadURL(
+  //   url.format({
+  //     pathname: path.join(__dirname, './src/pages/mode.tsx'),
+  //     protocol: 'file:',
+  //     slashes: true
+  //   })
+  // );
+}
+
+app.whenReady().then(createWindow);
