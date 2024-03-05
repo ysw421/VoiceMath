14dcbf7c0dd3767d539e603d36978fae3a11b579
siwon <ysw421@naver.com>
2024-03-06 08:32:48 +0900

    change electron setting: Full screen & use get port func

--- electron.js
+++ electron.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
+  const port = app.commandLine.getSwitchValue('port') || 3000;
+
   const win = new BrowserWindow({
+    // resizable: false,
+    fullscreen: true,
+
     width: 800,
     height: 600,
     webPreferences: {
@@ -9,14 +14,8 @@ function createWindow() {
     }
   });
 
-  win.loadURL('http://localhost:3000');
-  // win.loadURL(
-  //   url.format({
-  //     pathname: path.join(__dirname, './src/pages/mode.tsx'),
-  //     protocol: 'file:',
-  //     slashes: true
-  //   })
-  // );
+  process.stdout.write(`port: ${port}\n`);
+  win.loadURL(`http://localhost:${port}`);
 }
 
 app.whenReady().then(createWindow);
