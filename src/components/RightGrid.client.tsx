4d4a464df679194932d2e912d9deff0294c74223
Hoonsung Lee <Hoonsunglee5@gmail.com>
2024-04-02 22:12:18 +0900

    deleted buttons from front

--- src/components/RightGrid.client.tsx
+++ src/components/RightGrid.client.tsx
@@ -1,6 +1,6 @@
 import 'katex/dist/katex.min.css';
 
-import { evalCommandGetLabels, getLaTeXString, reset } from '@lib/commands';
+import { getLaTeXString } from '@lib/commands';
 import dialogflowToGeogebraCommand from '@lib/dialogflowToGeogebraCommand';
 import stt from '@lib/stt';
 import React, { useState } from 'react';
@@ -56,36 +56,8 @@ export default function RightGrid({
 
   return (
     <div className="tw-flex tw-flex-col tw-w-full tw-h-full">
-      <div className="tw-flex tw-flex-col tw-w-full tw-h-full tw-gap-y-4">
+      <div className="tw-flex tw-flex-col tw-w-full tw-h-auto tw-gap-y-4">
         <ScrollableLatex latexSentences={latexSentences} />
-        <form
-          className="tw-flex tw-flex-row tw-items-center tw-w-full"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (command === '') return;
-            else if (command === 'clear') {
-              reset(camera);
-              setLatexSentences([]);
-              setCommand('');
-              return;
-            }
-            var objLatex = evalCommandGetLabels(command);
-            if (objLatex == null) objLatex = command;
-            AddLatexSentence(objLatex);
-            setCommand('');
-          }}
-        >
-          <div className="tw-w-full tw-mr-3 tw-h-11">
-            <input
-              type="text"
-              value={command}
-              placeholder={'Type on the keyboard!'}
-              className="tw-w-full tw-h-full tw-px-2 tw-py-1 tw-border-2 tw-rounded-md"
-              onChange={(e) => setCommand(e.target.value)}
-            />
-          </div>
-          <Button> {'Submit'}</Button>
-        </form>
         <div className="tw-flex tw-flex-row tw-items-center tw-gap-x-3">
           <div>
             <AudioRecorder
@@ -95,24 +67,6 @@ export default function RightGrid({
                 echoCancellation: true
               }}
             />
-          </div>
-        </div>
-      </div>
-      <div className="tw-flex tw-flex-row-reverse tw-items-end tw-w-full tw-gap-x-4">
-        <div className="tw-flex tw-flex-row tw-gap-2">
-          <div className="tw-flex tw-flex-col tw-items-end tw-gap-y-2">
-            <Button
-              className="tw-w-32"
-              onClick={() => {
-                reset(camera);
-                setLatexSentences([]);
-              }}
-            >
-              Clear Space
-            </Button>
-          </div>
-          <div className="tw-flex tw-flex-col tw-items-end tw-gap-y-2">
-            <MoveBtn newPoint={defalutCamera} text={`Return to\nstarting point`} />
           </div>
         </div>
       </div>
